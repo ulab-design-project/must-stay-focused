389c9d9f9c8a87ad972d90713782b00031a645e8
zilchz0ne <zeesadearham@gmail.com>
2026-03-24 02:41:38 +0600

    Update flash_card.dart

--- lib/data/models/flash_card.dart
+++ lib/data/models/flash_card.dart
@@ -12,3 +12,50 @@
 //    - `int repetition = 0;` (SM-2 data)
 //    - `double easeFactor = 2.5;` (SM-2 data)
 // 2. Methods: toJson/fromJson.
+import 'package:isar/isar.dart';
+
+part 'flash_card.g.dart';
+
+@collection
+class FlashCard
+{
+  Id id = Isar.autoIncrement;
+
+  late String frontText;
+  late String backText;
+
+  late int deckId;
+
+  late DateTime nextReviewDate;
+
+  double interval = 1.0;
+  int repetition = 0;
+  double easeFactor = 2.5;
+
+  factory FlashCard.fromJson(Map<String, dynamic> json)
+  {
+    return FlashCard()
+      ..id = json['id'] ?? Isar.autoIncrement
+      ..frontText = json['front_text']
+      ..backText = json['back_text']
+      ..deckId = json['deck_id']
+      ..nextReviewDate = DateTime.parse(json['next_review_date'])
+      ..interval = (json['interval'] as num).toDouble()
+      ..repetition = json['repetition']
+      ..easeFactor = (json['ease_factor'] as num).toDouble();
+  }
+
+  Map<String, dynamic> toJson()
+  {
+    return {
+      'id': id,
+      'front_text': frontText,
+      'back_text': backText,
+      'deck_id': deckId,
+      'next_review_date': nextReviewDate.toIso8601String(),
+      'interval': interval,
+      'repetition': repetition,
+      'ease_factor': easeFactor,
+    };
+  }
+}
