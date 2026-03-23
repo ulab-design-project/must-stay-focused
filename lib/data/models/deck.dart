9aec52ec9f668a303ab7e09f4c61ed7c82183fc4
zilchz0ne <zeesadearham@gmail.com>
2026-03-24 02:38:12 +0600

    Update deck.dart

--- lib/data/models/deck.dart
+++ lib/data/models/deck.dart
@@ -7,3 +7,18 @@
 //    - `String name;`
 //    - `String subjectColorHex;` (e.g. '#FF0000' for quick visual ID) (FR-19).
 //    - `@Backlink` or relation to `List<FlashCard>`.
+import 'package:isar/isar.dart';
+import 'flash_card.dart';
+
+part 'deck.g.dart';
+
+@collection
+class Deck
+{
+  Id id = Isar.autoIncrement;
+
+  late String name;
+  late String subjectColorHex;
+
+  final flashCards = IsarLinks<FlashCard>();
+}
