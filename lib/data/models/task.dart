e3eccbba0850b02af647e9d2cccd812b43bd99e9
zilchz0ne <zeesadearham@gmail.com>
2026-03-24 02:42:33 +0600

    Update task.dart

--- lib/data/models/task.dart
+++ lib/data/models/task.dart
@@ -16,3 +16,69 @@
 //    - `bool isCompleted = false;`
 // 3. Methods:
 //    - `Map<String, dynamic> toJson()` and `factory Task.fromJson(...)` for exports.
+import 'package:isar/isar.dart';
+
+part 'task.g.dart';
+
+enum TaskPriority
+{
+  low,
+  medium,
+  high,
+  critical
+}
+
+enum TaskCategory
+{
+  essentials,
+  work,
+  study,
+  personal
+}
+
+@collection
+class Task
+{
+  Id id = Isar.autoIncrement;
+
+  late String title;
+  String? description;
+
+  DateTime? dueDate;
+  DateTime? time;
+
+  @enumerated
+  late TaskPriority priority;
+
+  @enumerated
+  late TaskCategory category;
+
+  bool isCompleted = false;
+
+  factory Task.fromJson(Map<String, dynamic> json)
+  {
+    return Task()
+      ..id = json['id'] ?? Isar.autoIncrement
+      ..title = json['title']
+      ..description = json['description']
+      ..dueDate = json['due_date'] != null ? DateTime.parse(json['due_date']) : null
+      ..time = json['time'] != null ? DateTime.parse(json['time']) : null
+      ..priority = TaskPriority.values.firstWhere((e) => e.name == json['priority'])
+      ..category = TaskCategory.values.firstWhere((e) => e.name == json['category'])
+      ..isCompleted = json['is_completed'];
+  }
+
+  Map<String, dynamic> toJson()
+  {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'due_date': dueDate?.toIso8601String(),
+      'time': time?.toIso8601String(),
+      'priority': priority.name,
+      'category': category.name,
+      'is_completed': isCompleted,
+    };
+  }
+}
